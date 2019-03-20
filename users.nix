3635ff4a39ab6cd9d4ef3fb1279cdea6a8da85ec
cmacrae <calum0macrae@gmail.com>
2019-03-20 17:52:15 +0000

    common: Fix sway auto-launch from TTY

--- users.nix
+++ users.nix
@@ -80,7 +80,7 @@ in
       . /etc/zshrc
       # If running from tty1 start sway
       if [ "$(tty)" = "/dev/tty1" ]; then
-        exec sway
+         /run/current-system/sw/bin/sway
       fi
     '';
 
