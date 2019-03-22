70f3705065da8896a256b243f9ae5a24a6454df1
cmacrae <calum0macrae@gmail.com>
2019-03-22 12:10:24 +0000

    common: Fix tmux config indentation

--- users.nix
+++ users.nix
@@ -215,59 +215,59 @@ in
       '';
     };
 
-  programs.tmux = {
-    enable = true;
-    shortcut = "q";
-    keyMode = "vi";
-    clock24 = true;
-    terminal = "screen-256color";
-    customPaneNavigationAndResize = true;
-    extraConfig = ''
-      unbind [
-      unbind ]
-      
-      bind ] next-window
-      bind [ previous-window
-      bind Escape copy-mode
-      
-      bind-key -r C-k resize-pane -U
-      bind-key -r C-j resize-pane -D
-      bind-key -r C-h resize-pane -L
-      bind-key -r C-l resize-pane -R
-      
-      bind-key -r C-M-k resize-pane -U 5
-      bind-key -r C-M-j resize-pane -D 5
-      bind-key -r C-M-h resize-pane -L 5
-      bind-key -r C-M-l resize-pane -R 5
-      
-      set -g pane-border-fg black
-      set -g pane-active-border-fg red
-      set -g display-panes-colour white
-      set -g display-panes-active-colour red
-      set -g display-panes-time 1000
-      set -g status-justify left
-      set -g set-titles on
-      set -g set-titles-string 'tmux: #T'
-      set -g repeat-time 100
-      set -g renumber-windows on
-      set -g renumber-windows on
-      
-      setw -g monitor-activity on
-      setw -g automatic-rename on
-      setw -g clock-mode-colour red
-      setw -g clock-mode-style 24
-      setw -g alternate-screen on
-      setw -g window-status-fg white
-      setw -g window-status-attr none
-      
-      set -g status-left ""
-      set -g status-right "#[fg=red,bg=default] %b %d #[fg=blue,bg=default] %R "
-      set -g status-right-length 100
-      set -g status-bg default
-      setw -g window-status-format "#[fg=blue,bg=black] #I #[fg=blue,bg=black] #W "
-      setw -g window-status-current-format "#[fg=blue,bg=default] #I #[fg=red,bg=default] #W "
-    '';
-  };
+    programs.tmux = {
+      enable = true;
+      shortcut = "q";
+      keyMode = "vi";
+      clock24 = true;
+      terminal = "screen-256color";
+      customPaneNavigationAndResize = true;
+      extraConfig = ''
+        unbind [
+        unbind ]
+        
+        bind ] next-window
+        bind [ previous-window
+        bind Escape copy-mode
+        
+        bind-key -r C-k resize-pane -U
+        bind-key -r C-j resize-pane -D
+        bind-key -r C-h resize-pane -L
+        bind-key -r C-l resize-pane -R
+        
+        bind-key -r C-M-k resize-pane -U 5
+        bind-key -r C-M-j resize-pane -D 5
+        bind-key -r C-M-h resize-pane -L 5
+        bind-key -r C-M-l resize-pane -R 5
+        
+        set -g pane-border-fg black
+        set -g pane-active-border-fg red
+        set -g display-panes-colour white
+        set -g display-panes-active-colour red
+        set -g display-panes-time 1000
+        set -g status-justify left
+        set -g set-titles on
+        set -g set-titles-string 'tmux: #T'
+        set -g repeat-time 100
+        set -g renumber-windows on
+        set -g renumber-windows on
+        
+        setw -g monitor-activity on
+        setw -g automatic-rename on
+        setw -g clock-mode-colour red
+        setw -g clock-mode-style 24
+        setw -g alternate-screen on
+        setw -g window-status-fg white
+        setw -g window-status-attr none
+        
+        set -g status-left ""
+        set -g status-right "#[fg=red,bg=default] %b %d #[fg=blue,bg=default] %R "
+        set -g status-right-length 100
+        set -g status-bg default
+        setw -g window-status-format "#[fg=blue,bg=black] #I #[fg=blue,bg=black] #W "
+        setw -g window-status-current-format "#[fg=blue,bg=default] #I #[fg=red,bg=default] #W "
+      '';
+    };
 
     programs.chromium = {
       enable = true;
