b517e262acd173f388b4793f7d5b171603ee6be0
cmacrae <calum0macrae@gmail.com>
2020-02-19 17:20:34 +0000

    machines/workbook: Add yabai bindings for display focus

--- machines/workbook/configuration.nix
+++ machines/workbook/configuration.nix
@@ -4,5 +4,12 @@
   local.darwin.skhd.extraBindings = ''
     cmd + ctrl - f : ~/.nix-profile/Applications/Firefox.app/Contents/MacOS/firefox -P work
     cmd + shift + ctrl - f : ~/.nix-profile/Applications/Firefox.app/Contents/MacOS/firefox -P home
+
+    cmd - up: yabai -m display --focus next
+    cmd - down: yabai -m display --focus prev
+    cmd +  ctrl - up: yabai -m window --display next
+    cmd +  ctrl - down: yabai -m window --display prev
+    cmd + shift + ctrl - up: yabai -m space --display next
+    cmd + shift + ctrl - down: yabai -m space --display prev
   '';
 }
