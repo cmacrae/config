43984c7b286318acbe1c174f998cf601f122a93c
cmacrae <hi@cmacr.ae>
2020-06-16 11:54:07 +0100

    yabai: Bump master revision to 7b9805c3

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,15 +16,13 @@ self: super: {
     inherit (super.darwin.apple_sdk.frameworks) vmnet;
   };
 
-  # NOTE: Following master until the new border implementation is released
-  #       https://github.com/koekeishiya/yabai/issues/565
   yabai = super.yabai.overrideAttrs (o: {
     version = "master";
     src = super.fetchFromGitHub {
       owner = "koekeishiya";
       repo = "yabai";
-      rev = "0690eb50c6e336345a1b1235b3e0baf0245b09d5";
-      sha256 = "148gjjn50l2x2psp37i37bfxgv31hmwqz5ryj6mvz16isffdx37z";
+      rev = "7b9805c3a33693d691b7551d4cdee5242ad4dae6";
+      sha256 = "01qqndawi4hl8lfvm11n7clla57hkrnck7k0hgsk6l62vyv26lj9";
     };
   });
 }
