c0be37d535c633a56e9f0fe95959253e236c5195
cmacrae <hi@cmacr.ae>
2020-12-02 17:58:12 +0000

    macintosh: Move to Nord theme

--- modules/macintosh.nix
+++ modules/macintosh.nix
@@ -113,33 +113,33 @@ in with pkgs.stdenv; with lib; {
     package = pkgs.yabai;
     enableScriptingAddition = true;
     config = {
-      window_border                = "on";
-      window_border_width          = 4;
-      active_window_border_color   = "0xff00afaf";
-      normal_window_border_color   = "0xff505050";
-      focus_follows_mouse          = "autoraise";
-      mouse_follows_focus          = "off";
-      window_placement             = "second_child";
-      window_opacity               = "off";
-      window_topmost               = "on";
-      window_shadow                = "float";
-      active_window_opacity        = "1.0";
-      normal_window_opacity        = "1.0";
-      split_ratio                  = "0.50";
-      auto_balance                 = "on";
-      mouse_modifier               = "fn";
-      mouse_action1                = "move";
-      mouse_action2                = "resize";
-      layout                       = "bsp";
-      top_padding                  = 10;
-      bottom_padding               = 10;
-      left_padding                 = 10;
-      right_padding                = 10;
-      window_gap                   = 10;
-      external_bar                 = "all:0:26";
+      window_border              = "on";
+      window_border_width        = 4;
+      active_window_border_color = "0xff81a1c1";
+      normal_window_border_color = "0xff3b4252";
+      focus_follows_mouse        = "autoraise";
+      mouse_follows_focus        = "off";
+      window_placement           = "second_child";
+      window_opacity             = "off";
+      window_topmost             = "on";
+      window_shadow              = "float";
+      active_window_opacity      = "1.0";
+      normal_window_opacity      = "1.0";
+      split_ratio                = "0.50";
+      auto_balance               = "on";
+      mouse_modifier             = "fn";
+      mouse_action1              = "move";
+      mouse_action2              = "resize";
+      layout                     = "bsp";
+      top_padding                = 10;
+      bottom_padding             = 10;
+      left_padding               = 10;
+      right_padding              = 10;
+      window_gap                 = 10;
+      external_bar               = "all:26:0";
     };
 
-    extraConfig = ''
+    extraConfig = mkDefault ''
       # rules
       yabai -m rule --add app='System Preferences' manage=off
       yabai -m rule --add app='Live' manage=off
@@ -151,18 +151,18 @@ in with pkgs.stdenv; with lib; {
   services.spacebar.package = pkgs.spacebar;
   services.spacebar.config = {
     debug_output       = "on";
-    position           = "bottom";
+    position           = "top";
     clock_format       = "%R";
     space_icon_strip   = "   ";
-    text_font          = ''"Menlo:Bold:12.0"'';
+    text_font          = ''"Roboto Mono:Regular:12.0"'';
     icon_font          = ''"FontAwesome:Regular:12.0"'';
-    background_color   = "0xff202020";
-    foreground_color   = "0xffa8a8a8";
-    space_icon_color   = "0xff14b1ab";
-    dnd_icon_color     = "0xfffcf7bb";
-    clock_icon_color   = "0xff99d8d0";
-    power_icon_color   = "0xfff69e7b";
-    battery_icon_color = "0xffffbcbc";
+    background_color   = "0xff2e3440";
+    foreground_color   = "0xffd8dee9";
+    space_icon_color   = "0xff81a1c1";
+    dnd_icon_color     = "0xff81a1c1";
+    clock_icon_color   = "0xff81a1c1";
+    power_icon_color   = "0xff81a1c1";
+    battery_icon_color = "0xff81a1c1";
     power_icon_strip   = " ";
     space_icon         = "";
     clock_icon         = "";
@@ -174,83 +174,6 @@ in with pkgs.stdenv; with lib; {
 
   # Recreate /run/current-system symlink after boot
   services.activate-system.enable = true;
-
-  # For use with nighthook:
-  # If there's no 'live.yml' alacritty config initially, copy it
-  # from the default config
-  environment.extraInit = ''
-    test -f ${homeDir}/.config/alacritty/live.yml || \
-      cp ${homeDir}/.config/alacritty/alacritty.yml \
-      ${homeDir}/.config/alacritty/live.yml
-  '';
-
-  launchd.user.agents.nighthook = {
-    serviceConfig = {
-      Label = "ae.cmacr.nighthook";
-      WatchPaths = [ "${homeDir}/Library/Preferences/.GlobalPreferences.plist" ];
-      EnvironmentVariables = {
-        PATH = (replaceStrings ["$HOME"] [homeDir] config.environment.systemPath);
-      };
-      ProgramArguments =  [
-        ''${pkgs.writeShellScript "nighthook-action" ''
-                if defaults read -g AppleInterfaceStyle &>/dev/null ; then
-                  MODE="dark"
-                else
-                  MODE="light"
-                fi
-                
-                emacsSwitchTheme () {
-                  if pgrep -q Emacs; then
-                    if [[  $MODE == "dark"  ]]; then
-                        emacsclient --eval "(cm/switch-theme 'doom-one)"
-                    elif [[  $MODE == "light"  ]]; then
-                        emacsclient --eval "(cm/switch-theme 'doom-solarized-light)"
-                    fi
-                  fi
-                }
-                
-                spacebarSwitchTheme() {
-                  if pgrep -q spacebar; then
-                    if [[  $MODE == "dark"  ]]; then
-                        spacebar -m config background_color 0xff202020
-                        spacebar -m config foreground_color 0xffa8a8a8
-                    elif [[  $MODE == "light"  ]]; then
-                        spacebar -m config background_color 0xffeee8d5
-                        spacebar -m config foreground_color 0xff073642
-                    fi
-                  fi 
-                }
-                
-                
-                alacrittySwitchTheme() {
-                  DIR=/Users/cmacrae/.config/alacritty
-                  if [[  $MODE == "dark"  ]]; then
-                    cp -f $DIR/alacritty.yml $DIR/live.yml
-                  elif [[  $MODE == "light"  ]]; then
-                    cp -f $DIR/light.yml $DIR/live.yml
-                  fi
-                }
-          
-                yabaiSwitchTheme() {
-                  if [[  $MODE == "dark"  ]]; then
-                    yabai -m config active_window_border_color "0xff5c7e81"
-                    yabai -m config normal_window_border_color "0xff505050"
-                    yabai -m config insert_window_border_color "0xffd75f5f"
-                  elif [[  $MODE == "light"  ]]; then
-                    yabai -m config active_window_border_color "0xff2aa198"
-                    yabai -m config normal_window_border_color "0xff839496 "
-                    yabai -m config insert_window_border_color "0xffdc322f"
-                  fi
-                }
-                
-                emacsSwitchTheme $@
-                spacebarSwitchTheme $@
-                alacrittySwitchTheme $@
-                yabaiSwitchTheme $@
-          ''}''
-      ];
-    };
-  };
 
   home-manager.users.cmacrae = {
     home.packages = (import ./packages.nix { inherit pkgs; });
@@ -312,7 +235,7 @@ in with pkgs.stdenv; with lib; {
           userChrome = (builtins.readFile (pkgs.substituteAll {
             name = "homeUserChrome";
             src = ../conf.d/userChrome.css;
-            tabLineColour = "#2aa198";
+            tabLineColour = "#5e81ac";
           }));
         };
 
@@ -325,7 +248,7 @@ in with pkgs.stdenv; with lib; {
           userChrome = (builtins.readFile (pkgs.substituteAll {
             name = "workUserChrome";
             src = ../conf.d/userChrome.css;
-            tabLineColour = "#cb4b16";
+            tabLineColour = "#d08770";
           }));
         };
       };
@@ -341,46 +264,11 @@ in with pkgs.stdenv; with lib; {
     programs.browserpass.enable = true;
     programs.browserpass.browsers = [ "firefox" ];
 
-    xdg.configFile."alacritty/light.yml".text =
-      let lightColours = {
-            colors = {
-              primary.background = "0xfdf6e3";
-              primary.foreground = "0x586e75";
-
-              normal = {
-                black = "0x073642";
-                red = "0xdc322f";
-                green = "0x859900";
-                yellow = "0xb58900";
-                blue = "0x268bd2";
-                magenta = "0xd33682";
-                cyan = "0x2aa198";
-                white = "0xeee8d5";
-              };
-
-              bright = {
-                black = "0x002b36";
-                red = "0xcb4b16";
-                green = "0x586e75";
-                yellow = "0x657b83";
-                blue = "0x839496";
-                magenta = "0x6c71c4";
-                cyan = "0x93a1a1";
-                white = "0xfdf6e3";
-              };
-            };
-          }; in
-        replaceStrings [ "\\\\" ] [ "\\" ]
-          (builtins.toJSON (
-            config.home-manager.users.cmacrae.programs.alacritty.settings
-            // lightColours
-          ));
-
     programs.alacritty = {
       enable = true;
       settings = {
-        window.padding.x = 15;
-        window.padding.y = 15;
+        window.padding.x = 24;
+        window.padding.y = 24;
         window.decorations = "buttonless";
         window.dynamic_title = false;
         scrolling.history = 100000;
@@ -389,34 +277,34 @@ in with pkgs.stdenv; with lib; {
         mouse.hide_when_typing = true;
 
         font = {
-          normal.family = "Menlo";
+          normal.family = "Roboto Mono";
           size = 12;
         };
 
         colors = {
-          primary.background = "0x282c34";
-          primary.foreground = "0xabb2bf";
+          primary.background = "#2e3440";
+          primary.foreground = "#d8dee9";
 
           normal = {
-            black = "0x282c34";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xabb2bf";
+            black   = "#3b4252";
+            red     = "#bf616a";
+            green   = "#a3be8c";
+            yellow  = "#ebcb8b";
+            blue    = "#81a1c1";
+            magenta = "#b48ead";
+            cyan    = "#88c0d0";
+            white   = "#e5e9f0";
           };
 
           bright = {
-            black = "0x5c6370";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xffffff";
+            black   = "#4c566a";
+            red     = "#bf616a";
+            green   = "#a3be8c";
+            yellow  = "#ebcb8b";
+            blue    = "#81a1c1";
+            magenta = "#b48ead";
+            cyan    = "#8fbcbb";
+            white   = "#eceff4";
           };
         };
 
