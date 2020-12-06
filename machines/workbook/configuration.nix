b9b7d770d392c2b1d4d399dc985948507cf5e04f
cmacrae <hi@cmacr.ae>
2020-12-06 23:00:53 +0000

    workbook: Remove old configuration

--- machines/workbook/configuration.nix
+++ machines/workbook/configuration.nix
@@ -5,8 +5,5 @@
 
   networking.hostName = "workbook";
   home-manager.users.cmacrae.programs.git.userName = "Calum MacRae";
-  home-manager.users.cmacrae.programs.git.userEmail = "calum.macrae@moo.com";
-  home-manager.users.cmacrae.programs.git.signing.signByDefault = false;
-  environment.etc."resolver/pantheon.cmacr.ae".text = "nameserver 10.0.0.2";
   services.spacebar.config.space_icon_strip = "    ";
 }
