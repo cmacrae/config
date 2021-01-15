e9e5c086c14ac603d2e9de277a0995698c2ff8e0
cmacrae <hi@cmacr.ae>
2021-01-15 18:07:35 +0000

    overlays: yabai 3.3.5 -> 3.3.6

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,17 +5,17 @@ self: super: {
 
   # TODO: Switch back to src build when SkyLight issue is fixed
   yabai = super.yabai.overrideAttrs (o: rec {
-    version = "3.3.5";
+    version = "3.3.6";
     src = builtins.fetchTarball {
       url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "195n2sdw25iw4xvy3ydlxh0x2i39ni04bsqjl7wcf7dh57w10bj9";
+      sha256 = "0a4yb1wisxhn7k8f9l4bp8swkb17qdkc4crh42zvz4lpaxg0sgxi";
     };
 
     installPhase = ''
       mkdir -p $out/bin
       mkdir -p $out/share/man/man1/
-      cp ./archive/bin/yabai $out/bin/yabai
-      cp ./archive/doc/yabai.1 $out/share/man/man1/yabai.1
+      cp ./bin/yabai $out/bin/yabai
+      cp ./doc/yabai.1 $out/share/man/man1/yabai.1
     '';
   });
 
