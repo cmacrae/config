0655a3c7d364e051cb07172fd58d95d395a1144d
cmacrae <hi@cmacr.ae>
2021-04-12 15:33:30 +0100

    macintosh: Only yabai external_bar padding for main display

--- macintosh.nix
+++ macintosh.nix
@@ -98,7 +98,7 @@ in
       left_padding = 10;
       right_padding = 10;
       window_gap = 10;
-      external_bar = "all:26:0";
+      external_bar = "main:26:0";
     };
 
     extraConfig = pkgs.lib.mkDefault ''
