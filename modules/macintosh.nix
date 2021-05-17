70097f91cb306e12a5fb29e02428094899f3b1fd
cmacrae <hi@cmacr.ae>
2021-05-17 16:11:39 +0100

    macintosh: Add personal binary cache

--- modules/macintosh.nix
+++ modules/macintosh.nix
@@ -25,13 +25,17 @@ in
   nix.trustedUsers = [ "root" "cmacrae" ];
 
   nix.binaryCaches = [
+    # Personal cache
+    "https://cachix.org/api/v1/cache/cmacrae"
     # Nightly Emacs build cache for github.com/cmacrae/emacs
     "https://cachix.org/api/v1/cache/emacs"
-    "https://cachix.org/api/v1/cache/nix-community"
+    # Binary cache for the srokell/deploy-rs binary
     "https://cachix.org/api/v1/cache/deploy-rs"
+    "https://cachix.org/api/v1/cache/nix-community"
   ];
 
   nix.binaryCachePublicKeys = [
+    "cmacrae.cachix.org-1:5Mp1lhT/6baI3eAqnEvruhLrrXE9CKe27SbnXqjwXfg="
     "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "deploy-rs.cachix.org-1:M+ZN++7fdqZFeIsvJyqeQrgnAbgsPNuv8z93uAJO43w="
