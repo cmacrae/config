ea64fa8187199ccfa25516fb529183ae5c8a47dd
Calum MacRae <hi@cmacr.ae>
2024-02-08 13:20:09 +0000

    midnight: grab firmware from webserver
    temporary workaround while we wait for a pure-eval solution upstream

--- nix/nixos/midnight/configuration.nix
+++ nix/nixos/midnight/configuration.nix
@@ -29,8 +29,14 @@
 
   hardware.opengl.enable = true;
   hardware.asahi = {
-    # TODO: how do we manage this purely?
-    peripheralFirmwareDirectory = ./firmware;
+    # TODO: awaiting upstream support for pure-eval solution.
+    #       for now, grab it from a temporary webserver rather
+    #       than tracking in git
+    # peripheralFirmwareDirectory = ./firmware;
+    peripheralFirmwareDirectory = builtins.fetchTarball {
+      url = "https://65c4a77cf00c00033cf84d2b--eclectic-horse-092596.netlify.app/firmware.tar.gz";
+      sha256 = "01swixbj1vyksm8h1m2ppnyxdfl9p7gqaxgagql29bysvngr8win";
+    };
 
     addEdgeKernelConfig = true;
     useExperimentalGPUDriver = true;
