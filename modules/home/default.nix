b767e1ff6ec04d366233c71596fe7917cdbdb8d2
Calum MacRae <hi@cmacr.ae>
2024-12-05 13:32:39 +0000

    style(home): nixpkgs-fmt

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -127,72 +127,76 @@ in
   programs.firefox.package =
     if isDarwin then
     # NOTE: firefox install is handled via homebrew
-      pkgs.runCommand "firefox-0.0.0" { } "mkdir $out"
+      pkgs.runCommand
+        "firefox-0.0.0"
+        { }
+        "mkdir $out"
     else
       pkgs.firefox;
   programs.firefox.languagePacks = [ "en-GB" ];
-  programs.firefox.profiles.home = mkMerge [
-    {
-      id = 0;
-      extensions = with inputs.firefox-addons.packages.${pkgs.stdenv.system}; [
-        browserpass
-        consent-o-matic
-        metamask
-        multi-account-containers
-        reddit-enhancement-suite
-        ublock-origin
-        vimium
-      ];
+  programs.firefox.profiles.home = mkMerge
+    [
+      {
+        id = 0;
+        extensions = with inputs.firefox-addons.packages.${pkgs.stdenv.system}; [
+          browserpass
+          consent-o-matic
+          metamask
+          multi-account-containers
+          reddit-enhancement-suite
+          ublock-origin
+          vimium
+        ];
 
-      search.default = "DuckDuckGo";
-      search.force = true;
+        search.default = "DuckDuckGo";
+        search.force = true;
 
-      settings = {
-        "app.update.auto" = false;
-        "app.normandy.enabled" = false;
-        "beacon.enabled" = false;
-        "browser.startup.homepage" = "https://lobste.rs";
-        "browser.search.region" = "GB";
-        "browser.search.countryCode" = "GB";
-        "browser.search.hiddenOneOffs" = "Google,Amazon.com,Bing";
-        "browser.search.isUS" = false;
-        "browser.ctrlTab.recentlyUsedOrder" = false;
-        "browser.newtabpage.enabled" = false;
-        "browser.bookmarks.showMobileBookmarks" = true;
-        "browser.uidensity" = 1;
-        "browser.urlbar.update" = true;
-        "datareporting.healthreport.service.enabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "distribution.searchplugins.defaultLocale" = "en-GB";
-        "extensions.getAddons.cache.enabled" = false;
-        "extensions.getAddons.showPane" = false;
-        "extensions.pocket.enabled" = false;
-        "extensions.webservice.discoverURL" = "";
-        "general.useragent.locale" = "en-GB";
-        "identity.fxaccounts.account.device.name" = osConfig.networking.hostName;
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.donottrackheader.value" = 1;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.cryptomining.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-        "privacy.trackingprotection.socialtracking.annotate.enabled" = true;
-        "reader.color_scheme" = "auto";
-        "services.sync.declinedEngines" = "addons,passwords,prefs";
-        "services.sync.engine.addons" = false;
-        "services.sync.engineStatusChanged.addons" = true;
-        "services.sync.engine.passwords" = false;
-        "services.sync.engine.prefs" = false;
-        "services.sync.engineStatusChanged.prefs" = true;
-        "signon.rememberSignons" = false;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.rejected" = true;
-        "toolkit.telemetry.updatePing.enabled" = false;
-      };
-    }
-  ];
+        settings = {
+          "app.update.auto" = false;
+          "app.normandy.enabled" = false;
+          "beacon.enabled" = false;
+          "browser.startup.homepage" = "https://lobste.rs";
+          "browser.search.region" = "GB";
+          "browser.search.countryCode" = "GB";
+          "browser.search.hiddenOneOffs" = "Google,Amazon.com,Bing";
+          "browser.search.isUS" = false;
+          "browser.ctrlTab.recentlyUsedOrder" = false;
+          "browser.newtabpage.enabled" = false;
+          "browser.bookmarks.showMobileBookmarks" = true;
+          "browser.uidensity" = 1;
+          "browser.urlbar.update" = true;
+          "datareporting.healthreport.service.enabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "distribution.searchplugins.defaultLocale" = "en-GB";
+          "extensions.getAddons.cache.enabled" = false;
+          "extensions.getAddons.showPane" = false;
+          "extensions.pocket.enabled" = false;
+          "extensions.webservice.discoverURL" = "";
+          "general.useragent.locale" = "en-GB";
+          "identity.fxaccounts.account.device.name" = osConfig.networking.hostName;
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.donottrackheader.value" = 1;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.cryptomining.enabled" = true;
+          "privacy.trackingprotection.fingerprinting.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.trackingprotection.socialtracking.annotate.enabled" = true;
+          "reader.color_scheme" = "auto";
+          "services.sync.declinedEngines" = "addons,passwords,prefs";
+          "services.sync.engine.addons" = false;
+          "services.sync.engineStatusChanged.addons" = true;
+          "services.sync.engine.passwords" = false;
+          "services.sync.engine.prefs" = false;
+          "services.sync.engineStatusChanged.prefs" = true;
+          "signon.rememberSignons" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.rejected" = true;
+          "toolkit.telemetry.updatePing.enabled" = false;
+        };
+      }
+    ];
 
   programs.emacs-twist = {
     enable = true;
